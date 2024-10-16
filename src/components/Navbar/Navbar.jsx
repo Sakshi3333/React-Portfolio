7795cfbfa16780149521cfcf938e90af83114bbd
Sakshi Shinde <95010031+Sakshi3333@users.noreply.github.com>
2024-10-16 11:34:08 +0530

    images

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <nav className={StyleSheet.navbar}>
+            <a href="/">Portfolio</a>
+            <div>
+                <ul>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#about">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
